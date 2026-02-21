c69893edd8f9ab31709540bca54efe67f7266b25
fabihasohail <msohailfabiha@gmail.com>
2026-02-21 12:58:07 +0500

    commit

---
+++ chap01/app.js
@@ -0,0 +1,22 @@
+// CHAP NO 01
+
+// ANS NO 01
+        // alert("Hello!");
+
+// ANS NO 02
+
+        //   alert("Error! please enter a valid password");
+
+//  ANS N0 03
+    // alert("Welcome to JS land \n happy coding!");
+
+
+//  ANS N0 04
+    // alert("Welcome to JS land ");
+    // alert(" happy coding! prevent this page from creating additional dialogs");
+
+
+
+//  ANS N0 05
+
+    // console.log("hello....I can run JS through my web browers's console");
