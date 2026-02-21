fb15176f72f8bd312a69bd403f08ebaae3c28d29
fabihasohail <msohailfabiha@gmail.com>
2026-02-21 23:24:25 +0500

    CHAP NO 02 "DONE"

---
+++ chap01/chap02/app.js
@@ -0,0 +1,24 @@
+
+
+// ANS NO 01
+var userName="fabihasohail@gmail.com";
+
+// ANS NO 02
+var myName="Fabiha M Sohail";
+
+// ANS NO 03
+var message="Hello world!";
+alert(message);
+
+// ANSNO 04
+var studentName="Jhone Doe ";
+var age=15;
+var skills="Certified Mobile Applicaition Development";
+alert(studentName);
+alert(age);
+alert(skills);
+
+
+// ANS N0 05
+var food="pizza\n pizz \n piz \n pi \n p";
+alert(food);
