5009e2271e1d77001567796930ef6a42143094d5
fabihasohail <msohailfabiha@gmail.com>
2026-02-28 13:35:41 +0500

    chap03 "DONE"

---
+++ chap03/app.js
@@ -0,0 +1,19 @@
+// CHAP NO 03
+
+// ANS NO 01:
+let age=17;
+alert(age);
+
+// ANS NO 02:
+let track=20;
+alert("I have visited this site"+" "+track+" "+"times")
+
+// ANS NO 03;
+let birth$year =2008;
+document.write("My birth year is"+" "+birth$year );
+document.write("<br />")
+document.write(typeof(birth$year));
+document.write("<br />")
+
+// ANS NO 04
+document.write("elSa ordered 10 Tea-coffee at islamabad resturant")
