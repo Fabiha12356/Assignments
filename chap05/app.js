7acc326b8f303fd3f7452721a72a9606867552b5
fabihasohail <msohailfabiha@gmail.com>
2026-03-10 07:12:10 +0500

    chap # 05

---
+++ chap05/app.js
@@ -0,0 +1,115 @@
+//    CHAP NO 05
+
+
+// ANS NO 01:-
+let num1= 3;
+let num2= 5;
+let sum= num1+num2;
+document.write("sum of"+" " + num1 + " " +"and"+" " + num2  + " "+ "is" +" " + sum );
+document.write("</br >")
+
+//    ANS N0 02:-
+let sum1= num1-num2;
+document.write("sum of"+" " + num1 + " " +"and"+" " + num2  + " "+ "is" +" " + sum1 );
+document.write("</br >")
+
+let sum2= num1*num2;
+document.write("sum of"+" " + num1 + " " +"and"+" " + num2  + " "+ "is" +" " + sum2 );
+document.write("</br >")
+
+let sum3= num1%num2;
+document.write("sum of"+" " + num1 + " " +"and"+" " + num2  + " "+ "is" +" " + sum3 );
+document.write("</br >")
+
+let sum4= num1/num2;
+document.write("sum of"+" " + num1 + " " +"and"+" " + num2  + " "+ "is" +" " + sum4 );
+document.write("</br >")
+
+//ANS N0 03:-
+// a:-
+let a;
+
+//b:-
+document.write("value after declaration is "+a);
+document.write("</br >")
+
+//c:-
+a=5;
+
+//d:-
+document.write("initail value "+a);
+document.write("</br >")
+
+//e:-
+a++
+
+//f:-
+document.write("after incremnet the value"+ " "+a);
+document.write("</br >")
+
+//g:-
+ a = a+7 ;
+
+ //h:-
+ document.write("after incremnet the value"+ " "+a);
+ document.write("</br >")
+
+ //i:-
+ a--
+
+ //j:-
+ document.write("after decremnet the value"+ " "+a);
+document.write("</br >")
+
+//L:-
+a= a%3;
+document.write("The Remainder is :"+ " "+a);
+document.write("</br >")
+
+
+
+
+//ANS NO 04:-
+let ticket1=600;
+let ticket2=5;
+let tickets=ticket1*ticket2;
+
+document.write("Total cost to buy" +" "+ ticket1 + " "+"to a movie is "+" "+ tickets +"PKR");
+document.write("</br >")
+
+//ANS NO 05:-
+document.write("Table of 4")
+document.write("</br >")
+let table=4;
+let  j=1;
+
+document.write(table +"x" + j +" =" +" "+ table);
+document.write("</br >")
+j++
+document.write(table +"x" + j +" =" +" "+ table*j);
+document.write("</br >")
+j++
+document.write(table +"x" + j +" =" +" "+ table*j);
+document.write("</br >")
+j++
+document.write(table +"x" + j +" =" +" "+ table*j);
+document.write("</br >")
+j++
+document.write(table +"x" + j +" =" +" "+ table*j);
+document.write("</br >")
+j++
+document.write(table +"x" + j +" =" +" "+ table*j);
+document.write("</br >")
+j++
+document.write(table +"x" + j +" =" +" "+ table*j);
+document.write("</br >")
+j++
+document.write(table +"x" + j +" =" +" "+ table*j);
+document.write("</br >")
+j++
+document.write(table +"x" + j +" =" +" "+ table*j);
+document.write("</br >")
+j++
+document.write(table +"x" + j +" =" +" "+ table*j);
+document.write("</br >")
+
