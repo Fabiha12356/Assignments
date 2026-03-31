12a2adc111f7669109d60e5461dfbc9370c00daf
fabihasohail <msohailfabiha@gmail.com>
2026-03-31 23:04:40 +0500

    commit

---
+++ chap 14 to 16/app.js
@@ -0,0 +1,73 @@
+//CHAP NO 14 TO 16:-
+
+
+// ANS NO 01:-
+
+// let studentName=["altamash" ,"warda", "irha" , "Ayeza" , "Ayat fatima"];
+
+// console.log(studentName);
+
+//ANS NO 02:-
+
+
+// let studentName2=[
+//     { name :"fabiha"} , 
+//     { name :"fabiha"} , 
+//     { name :"hamna"} , 
+//     { name :"hamna"} , 
+   
+// ]
+
+// console.log(studentName2);
+
+
+//ANS N0 03:-
+
+// let STRING=["fabiha" ,"hamna", "fabiha" , "hamna" , "fabiha" , "hamna"];
+
+// console.log(STRING);
+
+
+//ANS N0 04:-
+
+// let number =[ 1, 2 ,3 ,4 ,5 ,6, 7 ,8];
+//  console.log(number);
+
+
+ //ANS N0 05:-
+// let boolean =[ true , false , true , false , true];
+// console.log(boolean);
+
+
+// ANS N0 06:-
+
+// let mix=["fabiha" , 20 , "october" , true];
+// console.log(mix);
+
+// ANS N0 07:-
+// let education=["SSC" ,"HSC" , "BSC" ,"BS" , "BCOME" , "MS" , "M.PHIL" , "PHD"];
+// document.write("Qualifications");
+// document.write("<br/>");
+// document.write(education.join("<br>"));
+
+//ANS N0 08:-
+document.write("<br/>");
+let program=["hussain" , "fabiha" , "hamna"];
+let num=[320 , 230 , 480];
+let PERCNT=["64%","46%","96%" ];
+
+
+// ANS N0 09:-
+
+let names=[120,240,210,230];
+console.log(names[0],names[2],names[3],names[1]);
+
+
+// ANS N0 10:-
+let city = ["karachi" , "quetta" , "peshawar" , "Islamabad" , "lahore"];
+console.log(city[3] , city[1]);
+
+// ANS N0 12:-
+
+let arr=["this" , "is" , "my" , "cat"];
+console.log(arr);
